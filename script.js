c283fa4d8c79fe4a778361d19bd1a2d3052d613f
jpate24 <68245978+jpate24@users.noreply.github.com>
2024-02-19 20:01:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,124 @@
+var camera, scene, renderer,
+    geometry, material, mesh;
+
+
+
+//code for creating smoke background from CDN
+ 
+init();
+animate(); 
+
+function init() {
+    stats = new Stats();
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+
+    clock = new THREE.Clock();
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    scene = new THREE.Scene();
+ 
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+    scene.add( camera );
+ 
+    geometry = new THREE.CubeGeometry( 200, 200, 200 );
+    material = new THREE.MeshLambertMaterial( { color: 0xaa6666, wireframe: false } );
+    mesh = new THREE.Mesh( geometry, material );
+    //scene.add( mesh );
+    cubeSineDriver = 0;
+ 
+    // textGeo = new THREE.PlaneGeometry(300,300);
+    THREE.ImageUtils.crossOrigin = ''; //Need this to pull in crossdomain images from AWS
+    // textMaterial = new THREE.MeshLambertMaterial({color: 0x00ffff, opacity: 1, transparent: true, blending: THREE.AdditiveBlending})
+    // text = new THREE.Mesh(textGeo,textMaterial);
+    // text.position.z = 800;
+    // scene.add(text);
+
+    light = new THREE.DirectionalLight(0xffffff,0.5);
+    light.position.set(-1,0,1);
+    scene.add(light);
+  
+    smokeTexture = THREE.ImageUtils.loadTexture('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');
+    smokeMaterial = new THREE.MeshLambertMaterial({color: 0x45fc03, map: smokeTexture, transparent: true});
+    smokeGeo = new THREE.PlaneGeometry(300,300);
+    smokeParticles = [];
+
+
+    for (p = 0; p < 150; p++) {
+        var particle = new THREE.Mesh(smokeGeo,smokeMaterial);
+        particle.position.set(Math.random()*500-250,Math.random()*500-250,Math.random()*1000-100);
+        particle.rotation.z = Math.random() * 360;
+        scene.add(particle);
+        smokeParticles.push(particle);
+    }
+ 
+    document.body.appendChild( renderer.domElement );
+ 
+}
+ 
+function animate() {
+ 
+    // note: three.js includes requestAnimationFrame shim
+    stats.begin();
+    delta = clock.getDelta();
+    requestAnimationFrame( animate );
+    evolveSmoke();
+    render();
+    stats.end();
+}
+ 
+function evolveSmoke() {
+    var sp = smokeParticles.length;
+    while(sp--) {
+        smokeParticles[sp].rotation.z += (delta * 0.2);
+    }
+}
+
+function render() {
+ 
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.01;
+    cubeSineDriver += .01;
+    mesh.position.z = 100 + (Math.sin(cubeSineDriver) * 500);
+    renderer.render( scene, camera );
+ 
+}
+
+
+//code for countdown clock
+
+var countDownDate = new Date("Feb 25, 2024 00:00:00").getTime();
+
+// Update the countdown every second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Calculate time for days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the countdown is over, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
